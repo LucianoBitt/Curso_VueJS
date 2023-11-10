a05a06a7a4ffd08eea5dfcbc7a54c350c641c87c
Luciano Bittencourt <lucianobitt@hotmail.com>
2023-11-10 17:59:36 -0300

    Aula vue cli

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    {{ titulo }}
+    <v-btn color="secondary" @click="titulo += ' #'">Alterar</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      titulo: 'Teste Data usando Vue JS!!'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  /* background-color: chocolate; */
+  /* color: white; */
+}
+</style>
---
+++ vue-cli/projeto-simples/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Projeto-Simples</h1>
+    {{ nome }}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home-vue',
+  data() {
+    return {
+      nome: 'Vue CLI'
+    }
+  },
+  components: {
+
+  },
+}
+</script>
