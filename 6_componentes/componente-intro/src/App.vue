e7d5afec95162d8344ef321c0894e5a077f36b25
Luciano Bittencourt <lucianobitt@hotmail.com>
2023-11-16 17:28:52 -0300

    aulas componentes

---
+++ 6_componentes/componente-intro/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>Contadores</h1>
+    <app-contador />
+    <hr>
+    <app-contador />
+    <hr>
+    <app-contador />
+    <hr>
+    <app-contador />
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
