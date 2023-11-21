468f1a0b7a213406b73e5c52713a3543eb571dd5
Luciano Bittencourt <lucianobitt@hotmail.com>
2023-11-21 14:05:15 -0300

    desafio componente

--- 6_componentes/componente-intro/src/Contador.vue
+++ 6_componentes/componente-intro/src/Contador.vue
@@ -11,8 +11,8 @@ export default {
 	name: 'contador-component',
 	data() {
 		return {
-				contador: 0
-			}
+			contador: 0
+		}
 	},
 	methods: {
 		adicionar() {
@@ -25,20 +25,20 @@ export default {
 }
 </script>
 
-<style>
-	span {
-		border-bottom: 2px solid rgba(0, 4, 255, 0.26);
-		height: 30px;
-		padding: 5px 22px;
-	}
+<style scoped>
+span {
+	border-bottom: 2px solid rgba(0, 4, 255, 0.26);
+	height: 30px;
+	padding: 5px 22px;
+}
 
-	button {
-		height: 30px;
-		width: 30px;
-		border-radius: 15px;
-		background-color: rgba(255, 0, 0, 0.616);
-		color: #fff;
-		margin-left: 10px;
-		outline: none;
-	}
+button {
+	height: 30px;
+	width: 30px;
+	border-radius: 15px;
+	background-color: rgba(255, 0, 0, 0.616);
+	color: #fff;
+	margin-left: 10px;
+	outline: none;
+}
 </style>
