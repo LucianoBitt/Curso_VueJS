d8b578179ab5e8d783db3a99796fa66d70665068
Luciano Bittencourt <lucianobitt@hotmail.com>
2023-11-23 17:46:06 -0300

    aulas componente usuário

---
+++ 7_conponentes-comunicação/comunicacao/src/components/Usuario.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="container">
+        <h1>Componente Usuário</h1>
+        <p>Esse é um componente muito legal!</p>
+        <p>O nome correto é <strong>{{ nome }}</strong></p>
+        <button @click="alterarNome">Alterar Nome</button>
+        <hr>
+        <div class="componentes">
+            <app-usuario-info 
+            :nome="nome" 
+            @nomeMudou="nome = $event" />
+            <app-usuario-editar />
+        </div>
+    </div>
+</template>
+
+<script>
+import AppUsuarioInfo from './UsuarioInfo'
+import AppUsuarioEditar from './UsuarioEditar'
+
+export default {
+    components: { AppUsuarioInfo, AppUsuarioEditar },
+    data() {
+        return {
+            nome: 'Luciano'
+        }
+    },
+    methods: {
+        alterarNome() {
+            this.nome = 'Stefania'
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .container {
+        background-color: #27363b;
+        color: #fff;
+        padding: 10px;
+    }
+
+    .container button {
+        background-color: rgba(21, 255, 0, 0.507);
+        border-color: rgb(255, 255, 255);
+    }
+    .container hr {
+        margin: 20px 10px;
+    }
+
+    .componentes {
+        display: flex;
+    }
+
+    .componentes > * {
+        margin: 10px;
+    }
+    
+</style>
---
+++ 7_conponentes-comunicação/comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Nome do Usuário: <strong>{{ inverterNome() }}</strong></p>
+        <button @click="reiniciarNome">Reiniciar Nome</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        nome: {
+            type: String,
+            required: true
+        }
+    },
+    methods: {
+        inverterNome() {
+            return this.nome.split('').reverse().join('')
+        },
+        reiniciarNome() {
+            this.nome = 'Luciano'
+            this.$emit('nomeMudou', this.nome)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+    .container button {
+        background-color: rgba(255, 208, 0, 0.507);
+        border-color: rgb(255, 255, 255);
+    }
+</style>
